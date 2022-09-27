e5cb2c0b7fe0cd2dd69fcf974b15bc4cfce673ce
ErJump <fedelijunior@gmail.com>
2022-09-27 14:27:15 +0200

    feat: :sparkles: Adds getPosts method to main component vue

--- resources/js/components/Main.vue
+++ resources/js/components/Main.vue
@@ -5,9 +5,33 @@
 </template>
 
 <script>
-export default {
-    name: 'Main',
-}
+    import axios from 'axios'
+
+    export default {
+        name: 'Main',
+        data: function() {
+            return {
+                posts: [],
+                activePage: 1,
+            }
+        },
+        methods: {
+            getPosts(){
+                axios.get('/api/posts')
+                    .then(response => {
+                        this.posts = response.data.results.data
+                        console.log(this.posts);
+                    }) 
+                    .catch(error => {
+                        console.log(error);
+                    })
+            },
+        },
+        created(){
+            this.getPosts()
+        }
+    }
+    
 </script>
 
 <style>

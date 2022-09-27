65cc2ed8a075afce30b280fa941c216032d4b845
ErJump <fedelijunior@gmail.com>
2022-09-27 14:07:42 +0200

    feat: :sparkles: Adds guest home + vue

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('guest.home');
 });
 
 Auth::routes();
@@ -28,3 +28,7 @@ Route::middleware('auth')
         Route::resource('posts', 'PostsController');
         Route::resource('tags', 'TagController');
     });
+
+Route::get('{any?}', function() {
+    return view('guest.home');
+})->where('any', '.*');
